2eb0e00143cd5d86d0e8e5bf7230c9cd59be6ed0
vincentlizotte1944834 <1944834@cstj.qc.ca>
2023-11-27 13:16:39 -0500

    ajout-sous-section

---
+++ src/components/tableaudebord/sectionsoustitre.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../assets/scss/components/tableaudebord/sectionsoustitre.scss"
+
+const sectionsoustitre = () => {
+  
+    return (
+        <section className="containersectionsoustitre">
+            <div>
+                <h1 className="titresectionsoustitre">titresection</h1>
+            </div>
+
+        </section>
+    );
+  };
+
+
+export default sectionsoustitre;
--- src/pages/PageTableauDeBord.jsx
+++ src/pages/PageTableauDeBord.jsx
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom";
 import "../assets/scss/pages/tableaudebord.scss";
 
 import SectionTitre from "../components/tableaudebord/sectiontitre";
+import SectionSousTitre from "../components/tableaudebord/sectionsoustitre";
 
 const PageTableauDeBord = () => {
   
     return (
       <div>
         <SectionTitre />
+        <SectionSousTitre />
+        <SectionSousTitre />
       </div>
     );
   };
