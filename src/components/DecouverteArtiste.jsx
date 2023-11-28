304cfa4ee47b432eed0a80cb125cde14b54cbc56
Kevin Le Bihan <2081286@cstj.qc.ca>
2023-11-28 14:25:53 -0500

    klb-relatedArtist

--- src/components/DecouverteArtiste.jsx
+++ src/components/DecouverteArtiste.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams,Link } from "react-router-dom";
 import fetchJsonp from "fetch-jsonp";
 import { useEffect, useState } from "react";
 import '../css/DecouverteArtiste.css'
@@ -19,31 +19,49 @@ import '../css/DecouverteArtiste.css'
 
 const DecouverteArtiste = () => {
     const [popularSongs,setPopularSongs] = useState([]);
+    const [relatedArtist, setRelatedArtist] = useState([])
     const nom = "";
     const {idArtist} = useParams();
     console.log(idArtist);
 
+
+    const topRelatedArtist = () => {
+        const url = `https://api.deezer.com/artist/${idArtist}/related?output=jsonp`
+        console.log(url);
+
+fetchJsonp(url)
+    .then(resp => resp.json())
+    .then(data =>{
+        setRelatedArtist(data.data || []);
+        console.log(data.data);
+        
+    })
+    .catch(error => {
+        console.error("Erreur lors de la recherche:", error);
+    })
+    }
+
     const topSongs = () =>{
-         const url = `https://api.deezer.com/artist/${idArtist}/top&output=jsonp`
-    console.log(url);
+        const url = `https://api.deezer.com/artist/${idArtist}/top?&output=jsonp`
+        console.log(url);
 
-    fetchJsonp(url)
-        .then(resp => resp.json())
-        .then(data =>{
-            setPopularSongs(data.data || []);
-            console.log(data.data);
-            
-        })
-        .catch(error => {
-            console.error("Erreur lors de la recherche:", error);
-        })
-
-       
+        fetchJsonp(url)
+            .then(resp => resp.json())
+            .then(data =>{
+                setPopularSongs(data.data || []);
+                console.log(data.data);
+                
+            })
+            .catch(error => {
+                console.error("Erreur lors de la recherche:", error);
+            })
     }
      useEffect(() => {
             if (idArtist !=null) {
+                setRelatedArtist([])
                 setPopularSongs([]);
                 topSongs();
+                topRelatedArtist();
                 console.log(popularSongs);
             }
         }, []);
@@ -73,6 +91,22 @@ const DecouverteArtiste = () => {
                     )
                 })
             }
+
+                <h1 className="h1">Les fans aiment aussi</h1>
+            <div className="relatedArtist">
+                {
+                    relatedArtist.map((data,id) => {
+                        console.log(data)
+                        return(
+                            
+                            <div cla>
+                                <img src={data.picture} alt="" />
+                                <h2>{data.name}</h2>
+                            </div>
+                        )
+                    }
+                )}
+            </div>
         </div>
     )
 }
