6192ac7e86f936006606148dd44e53c4b25a5540
Vincent Lizotte <1944834@cstj.qc.ca>
2023-12-03 15:21:02 -0500

    fix-pages

--- src/components/authentification/login.jsx
+++ src/components/authentification/login.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Login() {
   return (
     <div className="login">
-      <h1 className="login-title">Bienvenue!</h1>
+      <h1 className="login-title">Connexion</h1>
       <div className="login-options">
         <button className="login-button google">Login with Google</button>
         <button className="login-button facebook">Login with Facebook</button>
