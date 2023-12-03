5e6ff6185302d13ac786e74227c746b5ccd7eefb
Vincent Lizotte <1944834@cstj.qc.ca>
2023-12-03 02:31:31 -0500

    ajouts-login

--- src/components/authentification/login.jsx
+++ src/components/authentification/login.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 function Login() {
   return (
-    <div className="login-container">
-      <h1 className="login-title">Bienvenu!</h1>
+    <div className="login">
+      <h1 className="login-title">Bienvenue!</h1>
       <div className="login-options">
         <button className="login-button google">Login with Google</button>
         <button className="login-button facebook">Login with Facebook</button>
       </div>
-      <button className="confirmation-button">Confirm</button>
     </div>
   );
 }
 
 export default Login;
+
