6a4551c535fee6551940b247e63aaf5fde2f66c1
Vincent Lizotte <1944834@cstj.qc.ca>
2023-12-04 13:30:08 -0500

    fix-tableau-de-bord

---
+++ src/components/PageAcceuil/Acceuil.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Acceuil = () => {
+  
+    return (
+        <section className="containersectionautresoustitre">
+            <div>
+                <h1>lool</h1>
+            </div>
+
+        </section>
+    );
+  };
+
+
+export default Acceuil;
