3f4a50497ae83e7a7e8517e4d2e20cdf75b26546
Nic <1728062@cstj.qc.ca>
2023-12-04 19:29:20 -0500

    feature-petitPasPourLHommeMaisUnGrandPourLHumanite

--- src/components/Playbar/SliderPlaybarFull.jsx
+++ src/components/Playbar/SliderPlaybarFull.jsx
@@ -17,19 +17,22 @@ const SliderPlaybarFul = ({ progress, duration }) => {
 
   const mathValue = value <= 50 ? 50 - value : value - 50;
 
-  const width = windowDimensions.width; // Meme valeur que dans le css(width: 95vw)
+  const width = windowDimensions.width < 800 ? windowDimensions.width : 800; // Meme valeur que dans le css(width: 95vw)
   const heightSlider = 40; // Meme valeur que dans le css(height: 20px)
   const widthSliderknob = 30;
   const radius = width / 2;
-  const radiusCircle = window.innerWidth * 1.5;
   const cos = mathValue / 100;
 
   const opposite = Math.sin(Math.acos(cos)) * radius * 2;
-  console.log(opposite);
+
+  // degré fonctionne pas avec les cosinus
   const degree =
     value <= 50
-      ? Math.acos(cos) * (180 / Math.PI)
+      ? Math.acos(cos) 
+      * (180 / Math.PI)
       : 180 - Math.acos(cos) * (180 / Math.PI);
+
+  console.log(degree);
 
   const tailStyle = {
     background: `conic-gradient(
@@ -39,11 +42,13 @@ const SliderPlaybarFul = ({ progress, duration }) => {
   };
   const thumbContainerStyle = {
     transform: `translateY(calc(${
+
       -opposite +
-      width -
-      heightSlider / 2 + widthSliderknob/2 +
-      (((Math.abs(value - 50) / 100) * (width + heightSlider * 50 + widthSliderknob*25 -20*100)) / 100 )
-    }px  - 100%
+      width +
+      heightSlider / 2 -
+      widthSliderknob / 2 -
+      (Math.abs(value - 50) / 100) * 50
+    }px - 50%
       )) translateX(calc(${
         (value / 100) * (width * 0.85 - widthSliderknob) -
         radius * 0.85 +
@@ -53,12 +58,11 @@ const SliderPlaybarFul = ({ progress, duration }) => {
   const sliderStyle = {
     transform: `translateY(calc(${
       -opposite +
-      width * 0.85 -
-      heightSlider*1.5 -
-      (((Math.abs(value - 50) / 100) * (width + heightSlider*100)) / 100 )
-
-      // - Math.abs((value / 100 )-50) * heightSlider
-    }px - 100%)) translateX(-50%)`,
+      width +
+      heightSlider / 2 -
+      widthSliderknob / 2 -
+      (Math.abs(value - 50) / 100) * 50
+    }px - 50%)) translateX(-50%)`,
   };
 
   // Math.sqrt(window.innerWidth - window.innerWidth * 0.95) -
