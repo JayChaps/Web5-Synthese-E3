a9ae8448755a98dc0aef96d089a6909296b2b799
Nic <1728062@cstj.qc.ca>
2023-12-05 06:35:02 -0500

    fix-slider

--- src/components/Playbar/ChansonsSuivantes.jsx
+++ src/components/Playbar/ChansonsSuivantes.jsx
@@ -1,11 +1,11 @@
 import ChansonSuivante from "./ChansonSuivante";
 import Glider from "react-glider";
 import "glider-js/glider.min.css";
+import { useEffect, useState } from "react";
 const ChansonsSuivantes = () => {
   const settings = {
     slidesToShow: 3,
     draggable: true,
-    
   };
   return (
     <section className="chansonssuivantes">
