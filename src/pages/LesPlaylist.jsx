8f2cf06a44cc9b69170f74c60caa1b2f529e7649
Nic <1728062@cstj.qc.ca>
2023-12-06 23:03:47 -0500

    fix-css-playlist

--- src/pages/LesPlaylist.jsx
+++ src/pages/LesPlaylist.jsx
@@ -1,32 +1,30 @@
-import React from 'react'
+import React from "react";
 import { FaPen } from "react-icons/fa6";
 import { GrSubtractCircle } from "react-icons/gr";
 import { FaPlayCircle } from "react-icons/fa";
-import SliderPlaylists from '../components/Playlist/SliderPlaylists';
+import SliderPlaylists from "../components/Playlist/SliderPlaylists";
 
 const LesPlaylist = () => {
   const urlImg = "src/assets/img/jpg/placeholder.jpg";
   return (
-    <div className='lesplaylists'>
-
-    <SliderPlaylists />
+    <div className="lesplaylists">
+      <SliderPlaylists />
       <header>
         <section className="lesplaylists__infos">
-          <h1>NomPlaylist</h1>
-          <h1>NomPlaylist</h1>
-          <h1>NomPlaylist</h1>
-          <h1>NomPlaylist</h1>
-          <section className='lesplaylists__infos__icones'>
+          <div className="titreplaylist">
+            <h1>NomPlaylist</h1>
+            <h1>NomPlaylist</h1>
+            <h1>NomPlaylist</h1>
+            <h1>NomPlaylist</h1>
+          </div>
+          <section className="lesplaylists__infos__icones">
             <div className="lesplaylistcompte">
               <img src={urlImg} alt="" />
               <span></span>
             </div>
             <FaPen size={"2rem"} color="var(--noir)" />
             <GrSubtractCircle size={"2rem"} color="var(--noir)" />
-            <FaPlayCircle
-            size={"4rem"}
-            color="var(--noir)"
-          />
+            <FaPlayCircle size={"4rem"} color="var(--noir)" />
           </section>
           <div className="coverplaylist">
             <img src={urlImg} alt="" />
@@ -34,11 +32,10 @@ const LesPlaylist = () => {
             <img src={urlImg} alt="" />
             <img src={urlImg} alt="" />
           </div>
-
         </section>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default LesPlaylist
+export default LesPlaylist;
