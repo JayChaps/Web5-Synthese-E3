534e7ecb171d94158c546cb925d158527a0ee0c5
Vincent Lizotte <1944834@cstj.qc.ca>
2023-12-10 16:38:36 -0500

    fixes

--- src/components/PageAcceuil/Acceuil.jsx
+++ src/components/PageAcceuil/Acceuil.jsx
@@ -10,7 +10,7 @@ import PetitsPoints from "../petitspoints";
 const Acceuil = () => {
   
   return (
-    <div>
+    <div className="acceuil">
         <TitreAcceuil/>
         <CoucheInfo1/>
         <PetitsPoints/>
