288af13a3fcd0aa3bda00314151df4be6ddaa141
Nic <1728062@cstj.qc.ca>
2023-12-10 14:20:33 -0500

    feature-boutons-deco-et-themes

--- src/components/Profil.jsx
+++ src/components/Profil.jsx
@@ -1,30 +1,50 @@
-import { useAuth } from '../context/auhContext';
-import Playlist from '../pages/Playlist';
-
+import { useState } from "react";
+import { useAuth } from "../context/auhContext";
+import Playlist from "../pages/Playlist";
+import SliderPlaylists from "./Playlist/SliderPlaylists";
 
 const Profil = () => {
-    const { user, googleSignIn, logOut } = useAuth();
-    const userName = user?.displayName || 'No name available';
+  const { user, googleSignIn, logOut } = useAuth();
+  const [isThemeOpen, setIsThemeOpen] = useState(false);
+  const userName = user?.displayName || "No name available";
 
-  
-    return (
-      <div>
-        <h1>{userName}</h1>
-        <h2>{user.email}</h2>
-        <button onClick={user === null ? googleSignIn : logOut}>
-            {user === null ? "Se connecter avec Google" : "Se déconnecter"}
-          </button>
-        <Playlist/>
-        <h2>Chansons aimées : </h2>
-        <div>
-            <h2>Vos thèmes : </h2>
-            <select name="theme" id="theme">
-            <option value="theme1">theme1</option>
-            <option value="theme2">theme2</option>
-        </select>
+  const handleTheme = () => {
+    setIsThemeOpen(!isThemeOpen);
+    console.log(isThemeOpen);
+  };
+  return (
+    <div className="profil">
+      <header>
+        <div className="boutonsprofil">
+          <div className={
+            isThemeOpen ? "open selecttheme" : "selecttheme"
+          } onClick={handleTheme}>
+            <span>Votre thème</span>
+            <ul name="theme" id="theme" className="themes">
+              <li value="theme1">Thème 1</li>
+              <li value="theme2">Thème 2</li>
+            </ul>
+          </div>
+
+          <div className="button-container">
+            <button
+              onClick={user === null ? googleSignIn : logOut}
+              className="button-acceuil-connexion"
+            >
+              {user === null ? "Se connecter avec Google" : "Se déconnecter"}
+            </button>
+          </div>
         </div>
-      </div>
-    );
+        <div className="nomcompte">
+          <h1>{userName}</h1>
+          <h2>{user.email}</h2>
+        </div>
+      </header>
+      <Playlist />
+      <SliderPlaylists />
+      <h2>Chansons aimées : </h2>
+    </div>
+  );
 };
 
 export default Profil;
