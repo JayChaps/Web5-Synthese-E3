ff20ad9c48c823d42e19ffd54cd4aa960360e52d
Vincent Lizotte <1944834@cstj.qc.ca>
2023-12-12 16:53:51 -0500

    feature-page-artistes-breakpoints-et-anims

--- src/components/DecouverteAlbum.jsx
+++ src/components/DecouverteAlbum.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams, Link } from "react-router-dom";
 import fetchJsonp from "fetch-jsonp";
+import { motion } from "framer-motion";
 import { SongInfoContext } from "../context/SongInfoContext";
 
 const DecouverteAlbum = () => {
@@ -18,29 +19,9 @@ const DecouverteAlbum = () => {
       const resp = await fetchJsonp(url);
       const data = await resp.json();
       setSongs(data.data || []);
-      console.log(data.data);
-      console.log(data.data);
 
       let tempContributors = {};
       let tracksTemp = [];
-
-      // const tracksPromise = data.data.map(track => {
-      //     return fetchJsonp(`https://api.deezer.com/track/${track.id}?output=jsonp`)
-      //     .then(response => response.json())
-      // });
-
-      // Promise.all(tracksPromise).then(results => {
-      //     console.log(results);
-      // });
-
-      // const tracksPromise = data.data.map(track => {
-      //     return fetchJsonp(`https://api.deezer.com/track/${track.id}?output=jsonp`)
-      //     .then(response => response.json())
-      // });
-
-      // Promise.all(tracksPromise).then(results => {
-      //     console.log(results);
-      // });
 
       for (let track of data.data) {
         const response = await fetchJsonp(
@@ -74,36 +55,37 @@ const DecouverteAlbum = () => {
   }, [idAlbum]);
 
   return (
-    <>
-      <div className="decouverte-album">
-        <div>
-          <h1>Album : {album.title}</h1>
-          {album.cover && <img src={album.cover_big} alt="image de l'album" />}
-        </div>
-
-        {album.artist && songs.length > 0 && (
-          <div className="decouverte">
-            <Link to={`/artist/${songs[0].artist.id}`}>
-              <div className="decouverte-artist">
-                <img src={album.artist.picture_small} alt={album.artist.name} />
-                <h1>{songs[0].artist.name}</h1>
-              </div>
-            </Link>
-          </div>
+    <motion.div className="decouverte-album" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+      <motion.div className="album-info" initial={{ x: -100, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 0.5 }}>
+        {album.cover && (
+          <motion.img src={album.cover_big} alt="image de l'album" className="album-cover" />
         )}
-      </div>
+        <div className="album-details">
+          <motion.h1 className="album-title-big">{album.title}</motion.h1>
+          {album.artist && songs.length > 0 && (
+            <div className="decouverte-artist">
+              <Link to={`/artist/${songs[0].artist.id}`}>
+                <div>
+                  <motion.img src={album.artist.picture_small} alt={album.artist.name} className="artiste-profil-img" />
+                  <motion.h1 className="artiste-name">{songs[0].artist.name}</motion.h1>
+                </div>
+              </Link>
+            </div>
+          )}
+        </div>
+      </motion.div>
 
-      <div>
-        <h1>Titres</h1>
+      <motion.div initial={{ x: 100, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 0.5 }}>
+        <h1 className="title-chansons">Titres:</h1>
         <div className="decouverte-songs">
           {songs.length > 0 &&
             songs.map((song, id) => (
-              <div className="song" key={id}>
+              <motion.div className="song" key={id} initial={{ x: -50, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 0.5 }}>
                 <div className="list">
-                  <h2>{song.track_position}</h2>
+                  <h2 className="titre-chanson-position">{song.track_position}</h2>
                 </div>
                 <div className="songInfos">
-                  <h3>{song.title}</h3>
+                  <h4 className="titre-chanson-speciale">{song.title}</h4>
                   <div className="nom">
                     {contributorsByTrack[song.id] &&
                       contributorsByTrack[song.id] && (
@@ -112,25 +94,21 @@ const DecouverteAlbum = () => {
                             (contributor, contributorId) => (
                               <div className="feature-div" key={contributorId}>
                                 <Link to={`/artist/${contributor.id}`}>
-                                  <h2 className="feature">
-                                    {contributor.name},
-                                  </h2>
+                                  <h2 className="feature">{contributor.name},</h2>
                                 </Link>
                               </div>
                             )
                           )}
-                          <button onClick={() => handlePlaySong(track[id])}>
-                            Lire
-                          </button>
+                          <button onClick={() => handlePlaySong(track[id])}>Lire</button>
                         </>
                       )}
                   </div>
                 </div>
-              </div>
+              </motion.div>
             ))}
         </div>
-      </div>
-    </>
+      </motion.div>
+    </motion.div>
   );
 };
 
