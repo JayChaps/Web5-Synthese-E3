71e4fc21543a9996dfad2c1f6e2ae9a5fbe9977d
Nic <1728062@cstj.qc.ca>
2023-12-12 22:07:47 -0500

    bizarre

--- src/pages/LesPlaylist.jsx
+++ src/pages/LesPlaylist.jsx
@@ -38,34 +38,32 @@ import { SongInfoContext } from "../context/SongInfoContext";
 import ItemPlaylist from "../components/Playlist/ItemPlaylist";
 
 const LesPlaylist = () => {
-  const [windowDimensions, setWindowDimensions] = useState(
-    window.innerWidth
-  );
   const urlImg = "/src/assets/img/jpg/placeholder.jpg";
 
   const { handlePlaySong } = useContext(SongInfoContext);
-  const { createNewPlaylist, deletePlaylist,
-    addToPlaylist, removeSongFromPlaylist,
-    newPlaylistName, setNewPlaylistName,
-    selectedPlaylistId, setSelectedPlaylistId,
-    selectedPlaylist, setSelectedPlaylist,
-    fetchPlaylists, fetchPlaylist,
-    playlists, setPlaylists,
-    playlist, setPlaylist,
-    selectedSong, setSelectedSong,
-    createNewPlaylistAndAddSong, 
-    clickedPlaylist, setClickedPlaylist } = useContext(PlaylistsContext);
-
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      setWindowDimensions(window.innerWidth);
-    });
-
-    return () => {
-      window.removeEventListener("resize", () => { });
-    };
-  }, []);
-
+  const {
+    createNewPlaylist,
+    deletePlaylist,
+    addToPlaylist,
+    removeSongFromPlaylist,
+    newPlaylistName,
+    setNewPlaylistName,
+    selectedPlaylistId,
+    setSelectedPlaylistId,
+    selectedPlaylist,
+    setSelectedPlaylist,
+    fetchPlaylists,
+    fetchPlaylist,
+    playlists,
+    setPlaylists,
+    playlist,
+    setPlaylist,
+    selectedSong,
+    setSelectedSong,
+    createNewPlaylistAndAddSong,
+    clickedPlaylist,
+    setClickedPlaylist,
+  } = useContext(PlaylistsContext);
 
   // Récupération des playlists
   // useEffect(() => {
@@ -75,7 +73,6 @@ const LesPlaylist = () => {
   //   }
   //   return () => (isSubscribed = false);
   // }, [selectedPlaylistId, fetchPlaylist]);
-
 
   // useEffect(() => {
   //   let isSubscribed = true;
@@ -89,11 +86,10 @@ const LesPlaylist = () => {
     fetchPlaylists();
   }, [fetchPlaylists]);
 
-
   const JouerLaChanson = (song) => {
     setSelectedSong(song);
     handlePlaySong(song);
-  }
+  };
 
   const handleDeleteSong = (playlistId, song) => {
     removeSongFromPlaylist(playlistId, song);
@@ -110,7 +106,6 @@ const LesPlaylist = () => {
   // useEffect(() => {
   //   setDefaultPlaylist(selectedPlaylist);
   // }, [selectedPlaylist]);
-  
 
   // useEffect(() => {
   //   if (clickedPlaylist && clickedPlaylist.id) {
@@ -120,7 +115,7 @@ const LesPlaylist = () => {
   //   }
   // }, [clickedPlaylist]);
 
-  // if(!clickedPlaylist || !clickedPlaylist.songs) 
+  // if(!clickedPlaylist || !clickedPlaylist.songs)
   // {
   //   console.log("ta mere");
   // }
@@ -130,119 +125,103 @@ const LesPlaylist = () => {
     setCreator(clickedPlaylist.creatorName);
   }, [clickedPlaylist]);
 
+  return (
+    <motion.div
+      className="lesplaylists"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <SliderPlaylists />
 
-  if (!clickedPlaylist || !clickedPlaylist.songs) {
-    
-    // If there is no clickedPlaylist or clickedPlaylist.songs, 
-    // set the first playlist of playlists as clickedPlaylist :
-    if (playlists && playlists.length > 0) {
-      setClickedPlaylist(playlists[0]);
-      console.log("setting the first playlist of playlists as clickedPlaylist");
-    }
-
-
-    console.log("playlist ", playlist);
-    console.log("playlists ", playlists);
-    
-    // Render a loading state or null if clickedPlaylist or clickedPlaylist.songs are undefined
-    return <div>Chargement...</div>;
-  } else {  
-    return (
-      <motion.div
-        className="lesplaylists"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-      >
-        <SliderPlaylists />
-
-        <header>
-          <motion.section
-            className="lesplaylists__infos"
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-          >
-            <div className="titreplaylist">
-              <div className="titreinner">
-                <h1>{plName}</h1>
-                <h1>{plName}</h1>
-                <h1>{plName}</h1>
-                <h1>{plName}</h1>
-              </div>
+      <header>
+        <motion.section
+          className="lesplaylists__infos"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+        >
+          <div className="titreplaylist">
+            <div className="titreinner">
+              <h1>{plName ? plName : "Créez votre playlist !"}</h1>
+              <h1>{plName ? plName : "Créez votre playlist !"}</h1>
+              <h1>{plName ? plName : "Créez votre playlist !"}</h1>
+              <h1>{plName ? plName : "Créez votre playlist !"}</h1>
             </div>
-            <motion.section
-              className="lesplaylists__infos__icones"
-              initial={{ opacity: 0, x: -50 }}
-              animate={{ opacity: 1, x: 0 }}
-            >
-              <div className="lesplaylistcompte">
-                <div>
-                  <img src={urlImg} alt="" />
-                </div>
-                <span>{creator}</span>
+          </div>
+          <motion.section
+            className="lesplaylists__infos__icones"
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+          >
+            
+            <div className="lesplaylistcompte">
+              <div>
+                <img src={urlImg} alt="" />
               </div>
-              <div className="lesplaylists__infos__icones__icones">
-                <FaPen size={"1rem"} color="var(--noir)" />
-                <CgRemove size={"1rem"} color="var(--noir)" />
-                <FaPlayCircle size={"2rem"} color="var(--noir)" />
-              </div>
-            </motion.section>
+              <span>{creator}</span>
+            </div>
+            <div className="lesplaylists__infos__icones__icones">
+              <FaPen size={"1rem"} color="var(--noir)" />
+              <CgRemove size={"1rem"} color="var(--noir)" />
+              <FaPlayCircle size={"2rem"} color="var(--noir)" />
+            </div>
           </motion.section>
+        </motion.section>
 
+        {
+          playlists && playlists.length > 0 &&
           <div className="coverplaylist">
             <img src={urlImg} alt="" />
             <img src={urlImg} alt="" />
             <img src={urlImg} alt="" />
             <img src={urlImg} alt="" />
           </div>
-        </header>
+        }
+      </header>
+
+      <motion.section
+        className="lesplaylists__playlist"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <Link to="/search">
+          <RechercheDeezerInput
+            placeholder={"Ajoutez une chanson !"}
+            icone={"add"}
+          />
+        </Link>
 
         <motion.section
-          className="lesplaylists__playlist"
+          className="lesplaylists__playlist__inner__chansons"
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
         >
-          <Link to="/search">
-            <RechercheDeezerInput
-              placeholder={"Ajoutez une chanson !"}
-              icone={"add"}
-            />
-          </Link>
-
-          <motion.section
-            className="lesplaylists__playlist__inner__chansons"
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
+          <div className="lesplaylists__playlist__inner__chansons__infos">
+            <span className="chansons__infos__number">#</span>
+            <span className="chansons__infos__titre">Titre</span>
+            <span className="chansons__infos__album">Album</span>
+            <span className="chansons__infos__duree">Durée</span>
+          </div>
+          <motion.div
+            className="lesplaylists__playlist__inner__chansons__chansons"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
           >
-            <div className="lesplaylists__playlist__inner__chansons__infos">
-              <span className="chansons__infos__number">#</span>
-              <span className="chansons__infos__titre">Titre</span>
-              <span className="chansons__infos__album">Album</span>
-              <span className="chansons__infos__duree">Durée</span>
-            </div>
-            <motion.div
-              className="lesplaylists__playlist__inner__chansons__chansons"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-            >
-              
-              {clickedPlaylist && clickedPlaylist.songs.map((song, index) => {
-                <>
-                  <AnimatedItem
-                    key={song.id}
-                  >
-                    <ItemChansons
-                      song={song}
-                      index={index}
-                      handleDeleteSong={handleDeleteSong}
-                      playlistId={clickedPlaylist.id}
-                      JouerLaChanson={JouerLaChanson}
-                      />
-                  </AnimatedItem>
-                </> 
-              })}
+            {playlist.songs.map((song, index) => {
+              return (
+                <AnimatedItem key={song.id}>
+                  <ItemChansons
+                    song={song}
+                    index={index}
+                    handleDeleteSong={handleDeleteSong}
+                    playlistId={clickedPlaylist.id}
+                    JouerLaChanson={JouerLaChanson}
+                  />
+                </AnimatedItem>
+              );
+            })}
 
-              {/* <AnimatedItem>
+            {/* <AnimatedItem>
                 <ItemChansons />
               </AnimatedItem>
 
@@ -273,163 +252,46 @@ const LesPlaylist = () => {
               <AnimatedItem delay={0.7}>
                 <ItemChansons />
               </AnimatedItem> */}
-
-            </motion.div>
-          </motion.section>
-
-          <div className="separateur">
-            <span>Chansons recommandées</span>
-          </div>
-
-          <motion.section
-            className="lesplaylists__playlist__inner__chansons__chansons recommande"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-          >
-            <AnimatedItem>
-              <ItemChansonRecomande premier={true} />
-            </AnimatedItem>
-            <AnimatedItem delay={0.1}>
-              <ItemChansonRecomande />
-            </AnimatedItem>
-            <AnimatedItem delay={0.2}>
-              <ItemChansonRecomande />
-            </AnimatedItem>
-            <AnimatedItem delay={0.3}>
-              <ItemChansonRecomande />
-            </AnimatedItem>
-            <AnimatedItem delay={0.4}>
-              <ItemChansonRecomande />
-            </AnimatedItem>
-            <AnimatedItem delay={0.5}>
-              <ItemChansonRecomande />
-            </AnimatedItem>
-            <AnimatedItem delay={0.6}>
-              <ItemChansonRecomande />
-            </AnimatedItem>
-            <AnimatedItem delay={0.7}>
-              <ItemChansonRecomande />
-            </AnimatedItem>
-          </motion.section>
+          </motion.div>
         </motion.section>
-      </motion.div>
-    );
-  } 
 
-  // return (
-  //   <div className="lesplaylists">
-  //     <SliderPlaylists />
-  //     {/* ... autres éléments de l'interface utilisateur */}
-  //     {playlists.map((playlist) => (
-  //       <div key={playlist.id}>
-  //         <h2>{playlist.name}</h2>
-  //         {/* Bouton pour supprimer une playlist */}
-  //         <button onClick={() => handleDeletePlaylist(playlist.id)}>Supprimer la playlist</button>
-  //         {/* Affichage des chansons */}
-  //         {playlist.songs.map((song, index) => (
-  //           <div key={song.id}>
-  //             {/* Affichez les détails de la chanson ici */}
-  //             <button onClick={() => JouerLaChanson(song)}>Lire</button>
-  //             <button onClick={() => handleDeleteSong(playlist.id, song)}>Supprimer</button>
-  //           </div>
-  //         ))}
-  //       </div>
-  //     ))}
-  //     {/* ... autres éléments de l'interface utilisateur */}
-  //   </div>
-  // );
+        <div className="separateur">
+          <span>Chansons recommandées</span>
+        </div>
 
-  // return (
-  //   <div className="lesplaylists">
-  //     <SliderPlaylists />
-  //     <header>
-  //       <section className="lesplaylists__infos">
-  //         <div className="titreplaylist">
-  //           <div className="titreinner">
-  //             <h1>NomPlaylist</h1>
-  //             <h1>NomPlaylist</h1>
-  //             <h1>NomPlaylist</h1>
-  //             <h1>NomPlaylist</h1>
-  //           </div>
-  //         </div>
-  //         <section className="lesplaylists__infos__icones">
-  //           <div className="lesplaylistcompte">
-  //             <div>
-  //               <img src={urlImg} alt="" />
-  //             </div>
-  //             <span>Nicolas Lauzon</span>
-  //           </div>
-  //           <div className="lesplaylists__infos__icones__icones">
-  //             <FaPen size={"1rem"} color="var(--noir)" />
-  //             <CgRemove size={"1rem"} color="var(--noir)" />
-  //             <FaPlayCircle size={"2rem"} color="var(--noir)" />
-  //           </div>
-  //         </section>
-  //       </section>
-  //       {/* Cover de la playlist 1 image dans chaque coin */}
-  //       <div className="coverplaylist">
-  //         <img src={urlImg} alt="" />
-  //         <img src={urlImg} alt="" />
-  //         <img src={urlImg} alt="" />
-  //         <img src={urlImg} alt="" />
-  //       </div>
-  //     </header>
-
-  //     <section className="lesplaylists__playlist">
-  //       <Link to="/search">
-  //         <RechercheDeezerInput
-  //           placeholder={"Ajoutez une chanson !"}
-  //           icone={"add"}
-  //         />
-  //       </Link>
-
-  //       <section className="lesplaylists__playlist__inner__chansons">
-  //         <div className="lesplaylists__playlist__inner__chansons__infos">
-  //           <span className="chansons__infos__number">#</span>
-  //           <span className="chansons__infos__titre">Titre</span>
-  //           <span className="chansons__infos__album">Album</span>
-  //           <span className="chansons__infos__duree">Durée</span>
-  //         </div>
-  //         <div className="lesplaylists__playlist__inner__chansons__chansons">
-
-  //           {/* faire boucle ici */}
-  //           {/* À passer:
-  //             numéro de la chanson
-  //             img
-  //             titre
-  //             artiste
-  //             album
-  //             durée
-  //           */}
-  //           <ItemChansons />
-  //           <ItemChansons />
-  //           <ItemChansons />
-  //           <ItemChansons />
-  //           <ItemChansons />
-  //           <ItemChansons />
-  //           <ItemChansons />
-  //           <ItemChansons />
-  //           <ItemChansons />
-  //           <ItemChansons />
-  //         </div>
-  //       </section>
-  //       <div className="separateur">
-  //         <span>Chansons recommandées</span>
-  //       </div>
-  //       <section className="lesplaylists__playlist__inner__chansons__chansons recommande">
-  //         {/* premier item a le props premier *Important* */}
-  //         <ItemChansonRecomande premier={true} />
-  //         <ItemChansonRecomande />
-  //         <ItemChansonRecomande />
-  //         <ItemChansonRecomande />
-  //         <ItemChansonRecomande />
-  //         <ItemChansonRecomande />
-  //         <ItemChansonRecomande />
-  //       </section>
-  //     </section>
-  //   </div>
-  // );
-
+        <motion.section
+          className="lesplaylists__playlist__inner__chansons__chansons recommande"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          <AnimatedItem>
+            <ItemChansonRecomande premier={true} />
+          </AnimatedItem>
+          <AnimatedItem delay={0.1}>
+            <ItemChansonRecomande />
+          </AnimatedItem>
+          <AnimatedItem delay={0.2}>
+            <ItemChansonRecomande />
+          </AnimatedItem>
+          <AnimatedItem delay={0.3}>
+            <ItemChansonRecomande />
+          </AnimatedItem>
+          <AnimatedItem delay={0.4}>
+            <ItemChansonRecomande />
+          </AnimatedItem>
+          <AnimatedItem delay={0.5}>
+            <ItemChansonRecomande />
+          </AnimatedItem>
+          <AnimatedItem delay={0.6}>
+            <ItemChansonRecomande />
+          </AnimatedItem>
+          <AnimatedItem delay={0.7}>
+            <ItemChansonRecomande />
+          </AnimatedItem>
+        </motion.section>
+      </motion.section>
+    </motion.div>
+  );
 };
 
 export default LesPlaylist;
