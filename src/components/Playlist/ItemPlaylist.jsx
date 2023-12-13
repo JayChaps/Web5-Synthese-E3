0793be83ca5bbf256883235d205cd0b22d6cb546
Jeremy Chaput <1964982@cstj.qc.ca>
2023-12-13 11:43:15 -0500

    fix-itemplaylist

--- src/components/Playlist/ItemPlaylist.jsx
+++ src/components/Playlist/ItemPlaylist.jsx
@@ -37,32 +37,84 @@ const ItemPlaylist = ({playlist, name, songs}) => {
   
   useEffect(() => {
     if (playlist && songs) {
-      const firstSong = theSongsArray[0];
-      if(firstSong) {setLaFirstSong(firstSong); setFirstCov(firstSong.album.cover);}
-      else if(!firstSong) { 
-        setFirstCov(urlImg); setSecondCov(urlImg); 
-        setThirdCov(urlImg); setFourthCov(urlImg); };
 
-      const secondSong = theSongsArray[1];
-      if(secondSong) {setLaSecondSong(secondSong); setSecondCov(secondSong.album.cover);}
-      else if(!secondSong) {setLaSecondSong(firstSong); setSecondCov(firstSong.album.cover);}
-      
-      const thirdSong = theSongsArray[2];
-      if(thirdSong) {setLaThirdSong(thirdSong); setThirdCov(thirdSong.album.cover);}
-      else if(!thirdSong) {
-        if(secondSong) {setLaThirdSong(secondSong); setThirdCov(secondSong.album.cover);}
-        else if(!secondSong) {setLaThirdSong(firstSong); setThirdCov(firstSong.album.cover);}
-      }
+      const firstSong = theSongsArray[0] ? theSongsArray[0] : null;
+      if (firstSong) {
+        setLaFirstSong(firstSong);
+        setFirstCov(firstSong.album.cover);
 
-      const fourthSong = theSongsArray[3];
-      if(fourthSong) {setLaFourthSong(fourthSong); setFourthCov(fourthSong.album.cover);}
-      else if(!fourthSong) {
-        if(thirdSong) {setLaFourthSong(thirdSong); setFourthCov(thirdSong.album.cover);}
-        else if(!thirdSong) {
-          if(secondSong) {setLaFourthSong(firstSong); setFourthCov(firstSong.album.cover);}
-          else if(!secondSong) {setLaFourthSong(firstSong); setFourthCov(firstSong.album.cover);}
+        const secondSong = theSongsArray[1] ? theSongsArray[1] : null;
+        if (secondSong) {
+          setLaSecondSong(secondSong);
+          setSecondCov(secondSong.album.cover);
+
+          const thirdSong = theSongsArray[2] ? theSongsArray[2] : null;
+          if (thirdSong) {
+            setLaThirdSong(thirdSong);
+            setThirdCov(thirdSong.album.cover);
+
+            const fourthSong = theSongsArray[3] ? theSongsArray[3] : null;
+            if (fourthSong) {
+              setLaFourthSong(fourthSong);
+              setFourthCov(fourthSong.album.cover);
+            } else if (!fourthSong) {
+              setLaFourthSong(firstSong);
+              setFourthCov(firstSong.album.cover);
+              console.log("no fourth song", playlist.name);
+              return;
+            }
+          } else if (!thirdSong) {
+            setLaThirdSong(firstSong);
+            setThirdCov(firstSong.album.cover);
+            console.log("no third song", playlist.name);
+            return;
+          } 
+        } else if (!secondSong) {
+          setLaSecondSong(firstSong);
+          setSecondCov(firstSong.album.cover);
+          console.log("no second song", playlist.name);
+          return;
         }
-      }
+      } else if (!firstSong) {
+        setFirstCov(urlImg);
+        setSecondCov(urlImg);
+        setThirdCov(urlImg);
+        setFourthCov(urlImg);
+        console.log("no first song", playlist.name);
+        return;
+      };
+
+
+
+      // const firstSong = theSongsArray[0] ? theSongsArray[0] : null;
+      // if(firstSong) {setLaFirstSong(firstSong); setFirstCov(firstSong.album.cover);}
+      // else if(!firstSong) { 
+      //   setFirstCov(urlImg); setSecondCov(urlImg); 
+      //   setThirdCov(urlImg); setFourthCov(urlImg); 
+      //   return;};
+
+      // const secondSong = theSongsArray[1] ? theSongsArray[1] : null;
+      // if(secondSong) {setLaSecondSong(secondSong); setSecondCov(secondSong.album.cover);}
+      // else if(!secondSong) {setLaSecondSong(firstSong); setSecondCov(firstSong.album.cover); return;}
+      
+      // const thirdSong = theSongsArray[2] ? theSongsArray[2] : null;
+      // if(thirdSong) {setLaThirdSong(thirdSong); setThirdCov(thirdSong.album.cover);}
+      // else if(!thirdSong) {
+      //   if(secondSong) {setLaThirdSong(secondSong); setThirdCov(secondSong.album.cover);}
+      //   else if(!secondSong) {setLaThirdSong(firstSong); setThirdCov(firstSong.album.cover); return;}
+      // }
+
+      // const fourthSong = theSongsArray[3] ? theSongsArray[3] : null;
+      // if(fourthSong) {setLaFourthSong(fourthSong); setFourthCov(fourthSong.album.cover);}
+      // else if(!fourthSong) {
+      //   if(thirdSong) {setLaFourthSong(thirdSong); setFourthCov(thirdSong.album.cover);}
+      //   else if(!thirdSong) {
+      //     if(secondSong) {setLaFourthSong(firstSong); setFourthCov(firstSong.album.cover);}
+      //     else if(!secondSong) {setLaFourthSong(firstSong); setFourthCov(firstSong.album.cover); return;}
+          
+      //   }
+      // }
+
     }
   }, [playlist]); // Add playlist as a dependency to the useEffect hook
   
