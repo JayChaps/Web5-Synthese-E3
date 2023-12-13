060c18a640538aa63a2ac270ee7dcad5d60bc35a
KevinLeBihan <2081286@cstj.qc.ca>
2023-12-13 14:16:50 -0500

    bonne-firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,25 +8,25 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 
-// // Le bon : 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCY0_e67aqCFxs2mgerWMJY9djaX5KeMBI",
-//     authDomain: "auth-web5-synthese-2023.firebaseapp.com",
-//     projectId: "auth-web5-synthese-2023",
-//     storageBucket: "auth-web5-synthese-2023.appspot.com",
-//     messagingSenderId: "482469779336",
-//     appId: "1:482469779336:web:7f5adfecb8a32d763d0c3d"
-// };
+// Le bon : 
+const firebaseConfig = {
+    apiKey: "AIzaSyCY0_e67aqCFxs2mgerWMJY9djaX5KeMBI",
+    authDomain: "auth-web5-synthese-2023.firebaseapp.com",
+    projectId: "auth-web5-synthese-2023",
+    storageBucket: "auth-web5-synthese-2023.appspot.com",
+    messagingSenderId: "482469779336",
+    appId: "1:482469779336:web:7f5adfecb8a32d763d0c3d"
+};
 
     // Le mien :
-const firebaseConfig = {
-    apiKey: "AIzaSyCO3LanYtBbIbp64UKSEETMqhHagJOP75w",
-    authDomain: "web5-synthese-temp.firebaseapp.com",
-    projectId: "web5-synthese-temp",
-    storageBucket: "web5-synthese-temp.appspot.com",
-    messagingSenderId: "413401273592",
-    appId: "1:413401273592:web:17a7a8d6703dff495d970d"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCO3LanYtBbIbp64UKSEETMqhHagJOP75w",
+//     authDomain: "web5-synthese-temp.firebaseapp.com",
+//     projectId: "web5-synthese-temp",
+//     storageBucket: "web5-synthese-temp.appspot.com",
+//     messagingSenderId: "413401273592",
+//     appId: "1:413401273592:web:17a7a8d6703dff495d970d"
+// };
 // Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
